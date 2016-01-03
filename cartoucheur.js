f6b498cd86a8af7eed42677bbb86fe20a6960300
gilou <contact+dev@gilouweb.com>
2016-01-03 15:58:27 +0100

    Remplacement des div/audio par d'un côté les players (audio), et de l'autre des buttons

--- cartoucheur.js
+++ cartoucheur.js
@@ -5,44 +5,49 @@ var players = [
 		{ id: "jingle4", source: "jingle4.mp3", type: "audio/mpeg" },
 	]
 
+/*
+ * Fill it with useless pads
+for(var i = 5; i < 13; i++){
+	players.push({ id: "jingle" + i, source: "jingle4.mp3", type: "audio/mpeg"});
+}
+*/
 var nbPlayers = players.length
 var cartoucheur = document.getElementById('cartoucheur')
 var bouton_stop = document.getElementById('stop')
+var timers = {}
 
 /**
  * What to do when audio file ends
  */
 function endedCallback(element){
-	element.parentElement.style.backgroundColor = 'rgb(220,0,0)';
-	element.parentElement.style.borderColor = 'rgb(255, 100, 100)';
-	element.parentElement.style.borderStyle = 'outset';
+	button = document.getElementById('button' + element.id);
+	button.style.backgroundColor = 'rgb(220,0,0)';
+	displayBouton(element);
 }
 
 function displayBouton(player){
-	etiquette = document.getElementById('label' + player.id);
+	etiquette = document.getElementById('button' + player.id);
 	etiquette.innerHTML = player.id +  "<br />"
 	etiquette.innerHTML += Math.round(player.currentTime*100)/100
-													+ " / "
-													+ Math.round(player.duration*100)/100;
+						+ " / "
+						+ Math.round(player.duration*100)/100;
 }
 
 /*
  * Create the <div><audio><source /></audio></div>
  * structure
  */
-function createAudioDiv(id, source, type) {
+function createAudioButton(id, source, type, rootElement) {
 	var container, player, s, etiquette
 	player = new Audio();
 	player.id = id
 	player.src = source;
 	player.preload = "auto";
-	container = document.createElement("div");
+	container = document.createElement("button");
+	container.id = 'button' + id
 	container.className = "cartouche";
-	container.appendChild(player);
-	etiquette = document.createElement("p");
-	etiquette.id = "label" + id;
-	etiquette.innerHTML = source;
-	container.appendChild(etiquette);
+	container.innerHTML = source;
+	rootElement.appendChild(player);
 
 
 	player.onended = function() {
@@ -52,9 +57,6 @@ function createAudioDiv(id, source, type) {
 		displayBouton(this);
 	}
 
-	player.ontimeupdate = function(){
-				displayBouton(this);
-	}
 	container.onclick = function() {
 		play(this);
 	}
@@ -63,20 +65,21 @@ function createAudioDiv(id, source, type) {
 }
 
 for(var i = 0; i < nbPlayers; i++){
-	container = createAudioDiv(players[i].id,
-											players[i].source,
-											players[i].type);
+	container = createAudioButton(players[i].id,
+					players[i].source,
+					players[i].type,
+					cartoucheur
+				     );
 
 	cartoucheur.insertBefore(container, bouton_stop);
 }
 
 function play(container) {
-	// normalement, on a <div><audio>
-	player = container.childNodes[0];
+	// normalement, on a <button id="buttonIDPLAYER">
+	player = document.getElementById(container.id.substr(6));
 	player.currentTime = 0;
-	container.style.borderStyle = 'inset';
-	container.style.borderColor = 'green';
 	container.style.backgroundColor = 'green';
+	timers[player.id] = setInterval(displayBouton, 50, player);
 	player.play();
 
 }
@@ -89,5 +92,8 @@ function stopAll(){
 		players[i].pause();
 		players[i].currentTime = 0;
 		endedCallback(players[i]);
+		if (timers[players[i].id] != undefined) {
+			window.clearInterval(timers[players[i].id])
+		}
 	}
 }
