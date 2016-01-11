b2a0cd8ade0a8a0a3f37374d86359607db5b2469
gilou <contact+dev@gilouweb.com>
2016-01-11 01:16:09 +0100

    OK boutons files, et affichage des noms

--- cartoucheur.js
+++ cartoucheur.js
@@ -1,19 +1,18 @@
-var players = [
-		{ source: "jingle1.wav", type: "audio/wav" },
-		{ source: "jingle2.wav", type: "audio/wav" },
-		{ source: "jingle3.mp3", type: "audio/mpeg" },
-		{ source: "jingle4.mp3", type: "audio/mpeg" },
-	]
-
+players = [ ];
+/* Pour les tests : */
+for(var iinit = 0; iinit < 16 ; iinit++){
+	players.push( { source: "" });
+}
 /*
  * Fill it with useless pads
 for(var i = 5; i < 13; i++){
-	players.push({ id: "jingle" + i, source: "jingle4.mp3", type: "audio/mpeg"});
+	players.push({ id: "jingle" + i, source: "", type: "audio/mpeg"});
 }
 */
 var nbPlayers = players.length
 var cartoucheur = document.getElementById('cartoucheur')
 var bouton_stop = document.getElementById('stop')
+var chargeur = document.getElementById('chargeur')
 var timers = {}
 
 /**
@@ -25,9 +24,13 @@ function endedCallback(element){
 	displayBouton(element);
 }
 
-function displayBouton(player){
+function displayBouton(player, texte){
+	if (texte == undefined){
+		texte = players[player.id.substr(6)].source;
+	}
+	
 	etiquette = document.getElementById('button' + player.id);
-	etiquette.innerHTML = player.id +  "<br />"
+	etiquette.innerHTML = texte +  "<br />"
 	etiquette.innerHTML += Math.round(player.currentTime*100)/100
 						+ " / "
 						+ Math.round(player.duration*100)/100;
@@ -37,11 +40,10 @@ function displayBouton(player){
  * Create the <div><audio><source /></audio></div>
  * structure
  */
-function createAudioButton(id, source, type, rootElement) {
+function createAudioButton(id, source, rootElement) {
 	var container, player, s, etiquette
 	player = new Audio();
 	player.id = id
-	player.src = source;
 	player.preload = "auto";
 	container = document.createElement("button");
 	container.id = 'button' + id
@@ -67,10 +69,11 @@ function createAudioButton(id, source, type, rootElement) {
 function onFile(evt){
 	file = evt.target.files[0];
 	url = URL.createObjectURL(file);
-	player = document.getElementById('player' + evt.target.id.substr(4));
+	id = evt.target.id.substr(4);
+	player = document.getElementById('player' + id);
 	if(player.canPlayType(file.type)){
-		console.log('can play' + file.name);
 		player.src = url;
+		players[id].source = file.name;
 	}
 	else {
 		console.log('fuck ' + file.name);
@@ -79,7 +82,6 @@ function onFile(evt){
 for(var i = 0; i < nbPlayers; i++){
 	container = createAudioButton('player' + i,
 					players[i].source,
-					players[i].type,
 					cartoucheur
 				     );
 
@@ -88,7 +90,7 @@ for(var i = 0; i < nbPlayers; i++){
 	file.type = 'file';
 	file.id = 'file' + i;
 	file.addEventListener('change', onFile, false);
-	cartoucheur.insertBefore(file, bouton_stop);
+	chargeur.appendChild(file);
 
 }
 /*
@@ -96,7 +98,10 @@ for(var i = 0; i < nbPlayers; i++){
  */
 function toggle(container){
 	player = document.getElementById(container.id.substr(6));
-	if(player.currentTime == 0 || player.paused){
+	if(player.src == ''){
+		return false;
+	}
+	if(player.currentTime == 0 || player.paused ){
 		container.style.backgroundColor = 'green';
 		play(player);
 	} else {
@@ -115,15 +120,18 @@ function stop(player){
 function play(player) {
 	player.currentTime = 0;
 	timers[player.id] = setInterval(displayBouton, 50, player);
+	player.load();
 	player.play();
 
 }
 
 function stopAll(){
 
-	var players = document.getElementsByTagName('audio');
-	playersLength = players.length;
+	var stopplayers = document.getElementsByTagName('audio');
+	playersLength = stopplayers.length;
 	for( var i = 0; i < playersLength; i++){
-		stop(players[i]);
+		if(stopplayers[i].src != ''){
+			stop(stopplayers[i]);
+		}
 	}
 }
